afb5847fc922e832fbc89753ebc23681bc6906be
unknown <nicoleta.flavia03@gmail.com>
2014-06-15 16:29:33 +0300

    process text from server

--- src/main/java/com/nlp/disambiguation/service/ProcessText.java
+++ src/main/java/com/nlp/disambiguation/service/ProcessText.java
@@ -1,59 +1,81 @@
 package com.nlp.disambiguation.service;
 
-import com.nlp.disambiguation.model.PartOfSpeech;
+import org.w3c.dom.*;
+import org.w3c.dom.Node;
 
-import java.util.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.soap.*;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /**
- * Created by Dell on 6/1/14.
+ * Created by Dell on 6/15/14.
  */
 public class ProcessText {
+    private static String getResponse(String input){
+        String result = null;
+        try{
+            String xmldata ="<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                    "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">\n" +
+                    "  <soap12:Body>\n" +
+                    "    <Process xmlns=\"http://nlp.racai.ro/WebServices\">\n" +
+                    "      <input>"+input+"</input>\n" +
+                    "      <lang>ro</lang>\n" +
+                    "    </Process>\n" +
+                    "  </soap12:Body>\n" +
+                    "</soap12:Envelope>";
 
-    public static Map<String, PartOfSpeech> getProcessedWords(String input, String output){
-        Map<String, PartOfSpeech> map = new HashMap<>();
-        String[] inputArr = input.split("\\s");
-        String[] outputArr = output.split("\\s");
+            String hostname = "tutankhamon.racai.ro";
+            int port = 80;
+            InetAddress address = InetAddress.getByName(hostname);
+            Socket socket = new Socket(address, port);
 
-        if(inputArr.length != outputArr.length){
-            return null;
-        } else {
-            int len = inputArr.length;
-            for(int i=0; i<len; i++){
-                String inputStr = inputArr[i];
-                String outputStr = outputArr[i];
-                String[] currentWord = outputStr.split("\\|");
-                if(inputStr.equals(currentWord[0])){
-                    String partOfSpeechTag = currentWord[2];
-                    if(partOfSpeechTag.length() >= 2){
-                        char firstPosition = partOfSpeechTag.charAt(0);
-                        char secondPosition = partOfSpeechTag.charAt(1);
-                        PartOfSpeech partOfSpeech = new PartOfSpeech();
-                        if(firstPosition == 'N' && secondPosition != 'P' ) {
-                            partOfSpeech.setSymbol('N');
-                            partOfSpeech.setName("noun");
-                        } else if (firstPosition == 'V' && secondPosition != 'A') {
-                            partOfSpeech.setSymbol('V');
-                            partOfSpeech.setName("verb");
-                        } else {
-                            continue;
-                        }
-                        map.put(currentWord[1], partOfSpeech);
-                    }
-                } else {
-                    return null;
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write("POST /webservices/TextProcessing.asmx HTTP/1.0\r\n");
+            bufferedWriter.write("Host: tutankhamon.racai.ro\r\n");
+            bufferedWriter.write("Content-Type: application/soap+xml; charset=\"utf-8\"\r\n");
+            bufferedWriter.write("Content-Length: " + xmldata.length() + "\r\n");
+            bufferedWriter.write("\r\n");
+
+            bufferedWriter.write(xmldata);
+            bufferedWriter.flush();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null){
+                if(line.startsWith("<?xml")){
+                    result = line;
                 }
             }
+        } catch (Exception e){
+            e.printStackTrace();
         }
-        return map;
+        return result;
     }
 
-    public static void main(String[] args){
-        String input = "Maria va merge la mare !";
-        String output = "Maria|Maria|NP|Np va|vrea|VA3S|Va--3s merge|merge|VN|Vmnp la|la|S|Spsa mare|mare|NSRN|Ncfsrn !|!|EXCL|EXCL";
-        Map<String, PartOfSpeech> map = ProcessText.getProcessedWords(input, output);
-        Set<String> keySet = map.keySet();
-        for(String key:keySet){
-            System.out.println("key: " + key + ";\t value: " + map.get(key));
+    private static String getProcessedText(String xml){
+        String result = null;
+        try{
+            DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+            DocumentBuilder b = f.newDocumentBuilder();
+            Document doc = b.parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
+            Node processResult = doc.getElementsByTagName("ProcessResponse").item(0);
+            result = processResult.getTextContent();
+
+        } catch (Exception e){
+            e.printStackTrace();
         }
+        return result;
     }
+
+    public static String processInputText(String input){
+        return getProcessedText(getResponse(input));
+    }
+
+//    public static void main(String[] args){
+//        System.out.println(processInputText("Maria va merge la mare !"));
+//    }
 }
